b29091f383ce27f37e78f5c53939ebaba9b97b16
CamilleMrntz <94170738+CamilleMrntz@users.noreply.github.com>
2023-04-03 10:49:10 +0200

    sum() function added

--- rollTheDice.js
+++ rollTheDice.js
@@ -278,3 +278,10 @@ Variable roll_score line 198 cannot carry 3 return ?
 
 
 
+function sum(array){
+    let sum = 0
+    for (let i = 0; i < array.length; i += 1) {
+    sum += array[i]
+    }
+    return sum
+  }
